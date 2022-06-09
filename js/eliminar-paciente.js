f4c3214d516aeadd21fbb9f1d0b74e2ce9fbc786
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-08 22:11:21 -0500

    Se agrega un alert al momento de eliminar un paciente

--- js/eliminar-paciente.js
+++ js/eliminar-paciente.js
@@ -3,6 +3,7 @@ var tabla = document.querySelector("#tabla-pacientes");
 tabla.addEventListener("dblclick",function(event){
     event.target.parentNode.classList.add("fadeOut");
     setTimeout(function(){
-        event.target.parentNode.remove();    
+        event.target.parentNode.remove();
+        alert("Paciente eliminado correctamente")    
     },500);
 }); 
